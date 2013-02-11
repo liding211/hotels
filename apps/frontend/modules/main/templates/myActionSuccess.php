145816d5840bd0dbb3b56fdf274f84e3c142a745
Khomenko Sergey <sergey.khomenko@postindustria.com>
2013-02-11 23:25:01 +0200

    Fixing form action in main module

--- apps/frontend/modules/main/templates/myActionSuccess.php
+++ apps/frontend/modules/main/templates/myActionSuccess.php
@@ -1,6 +1,6 @@
 <p>Test form!</p>
-<form method="post" action="/index.php/main/formAction">
-  <label for="name">What is your name?</label>
-  <input type="text" name="name" id="name" value="" />
-  <input type="submit" value="Ok" />
+<?php echo form_tag('main/formAction') ?>
+  <?php echo label_for('name', 'What is your name?') ?>
+  <?php echo input_tag('name') ?>
+  <?php echo submit_tag('Ok') ?>
 </form>
