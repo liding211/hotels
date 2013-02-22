0ca075af01cdd31c654a62f7ef908267f24c04e2
Khomenko Sergey <sergey.khomenko@postindustria.com>
2013-02-22 18:07:57 +0200

    Fixed code of filtering email has been moved from modules templates to main template.

--- apps/backend/modules/reservation/templates/_email_filter.php
+++ apps/backend/modules/reservation/templates/_email_filter.php
@@ -1,9 +1,7 @@
 <?php
-
-    $email = isset($filters['email']) ? $filters['email'] : '';
     
     include_partial('global/autocomplete_email', 
         array( 'input_name' => 'filters[email]', 
         'jquery_identifier' => 'filters_email',
-        'selected_value' => $email));
+        'selected_value' => isset($filters['email']) ? $filters['email'] : ''));
 ?>
