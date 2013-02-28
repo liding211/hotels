0111bdb11a5546b1f623f7e1e1f3db7ee593943e
Khomenko Sergey <sergey.khomenko@postindustria.com>
2013-02-28 14:23:16 +0200

    Fixed db connection, added sign in form

--- apps/frontend/modules/authentication/templates/indexSuccess.php
+++ apps/frontend/modules/authentication/templates/indexSuccess.php
@@ -1,3 +1,65 @@
-<p style="font-size: 24px; font-weight: bold;"><?php echo $text; ?></p>
-<p style="font-size: 20px;"><?php echo $description; ?></p>
-<?php echo link_to('Tell us about you', 'main/myAction'); ?>
+<div id="entry_block">
+	<div id="forms">
+	<!-- registration form -->
+	<div id="reg_form">
+		<p id="form_title">REGISTRATION</p>
+		<p>First time with us? Tell us about yourself!</p>
+		<form name="reg_form" class="reg">
+			<table>
+				<tr id="first_name">
+					<td>First name*</td>
+					<td>
+						<input type="textfield" name="first_name">
+					</td>
+				</tr>
+				<tr id="last_name">
+					<td>Last name*</td>
+					<td>
+						<input type="textfield" name="last_name">
+					</td>
+				</tr>
+				<tr id="email">
+					<td>E-mail*</td>
+					<td>
+						<input type="textfield" name="email">
+					</td>
+				</tr>
+				<tr id="phone">
+					<td>Phone number*</td>
+					<td>
+						<input type="textfield" name="phone">
+					</td>
+				</tr>
+				<tr id="reg_submit">
+					<td colspan=2 ><input type="button" id="reg" value="Registration" disabled></td>
+				</tr>
+			</table>
+		</form>
+	</div>
+	<!-- end of registration form -->
+	
+	<!-- login form -->
+	<div id="login_form">
+		<p id="form_title">SIGN</p>
+		<p>Regular customer? Enter the e-mail!</p>
+		<form name="login_form" action="index.php?action=signin" method="POST" class="login">
+			<table>
+				<tr id="client_email">
+					<td>E-mail</td>
+					<td>
+						<input type="textfield" id="email_login" name="email_login">
+					</td>
+				</tr>
+				<tr id="login_submit">
+					<td colspan=2 >
+						<input type="button" id="login" value="enter" disabled>
+					</td>
+				</tr>
+			</table>
+		</form>
+	</div>
+	<!-- end of login form -->
+	</div>
+</div>
+<script type="text/javascript" src="js/form_validater.js"></script>	
+<script type="text/javascript" src="js/enter.ajax.js"></script>
